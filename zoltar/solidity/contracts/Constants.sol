f33999cd51ca3ccf02a7c50f68ef4a5776a3644a
KillariDev <13102010+KillariDev@users.noreply.github.com>
2026-09-15 10:53:15 +0000

    Remove unused protocol configuration and Solidity constants

--- zoltar/solidity/contracts/Constants.sol
+++ zoltar/solidity/contracts/Constants.sol
@@ -2,9 +2,7 @@
 pragma solidity 0.8.35;
 
 library Constants {
-	uint48 constant YEAR_2099 = 4080321626;
 	uint88 constant MAX_ATTO_REP = 11_000_000e18;
-	uint256 constant NUM_OUTCOMES = 3;
 	uint256 constant MINIMUM_FORK_BURN_DIVISOR = 5;
 	address constant BURN_ADDRESS = 0xDeaDbeefdEAdbeefdEadbEEFdeadbeEFdEaDbeeF;
 }
